6540b6e48e6b74eb3f483f2e461334b86c375fbe
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-12-07 17:46:50 +0100

    post and get requests

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const express = require("express");
-
+const axios = require("axios");
+const port = 3000
 const app = express();
 
 app.use(express.json());
 
+app.get("/", (req, res) => {
+    res.status(200).send("<h1>This Works</h1>")
+})
+
 app.post("/", (req, res) => {
     const { country, city, query_string, api_key } = req.body;
-    return axios.post(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=${query_string} in ${city} in ${country}&radius=2000&key=${api_key}`)
+    console.log(req.body);
+    try {
+        return axios.post(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=${query_string} in ${city} in ${country}&radius=2000&key=${api_key}`)
+    } catch (e) {
+        console.log(e)
+    }
 })
 
 app.listen(3000, () => {
